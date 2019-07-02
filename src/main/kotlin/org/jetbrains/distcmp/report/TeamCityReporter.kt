04cf501a158ed92f506e3b1889769d962012a8c7
Sergey Rostov <sergey.rostov@jetbrains.com>
2019-07-02 16:38:55 +0300

    teamcity disable sync

--- src/main/kotlin/org/jetbrains/distcmp/report/TeamCityReporter.kt
+++ src/main/kotlin/org/jetbrains/distcmp/report/TeamCityReporter.kt
@@ -1,11 +1,12 @@
 package org.jetbrains.distcmp.report
 
 import jetbrains.buildServer.messages.serviceMessages.*
-import org.jetbrains.distcmp.*
+import org.jetbrains.distcmp.DiffSettings
+import org.jetbrains.distcmp.Item
+import org.jetbrains.distcmp.WorkManager
 import org.jetbrains.distcmp.report.FileStatus.*
 import java.io.PrintStream
 import java.io.PrintWriter
-import java.util.concurrent.Future
 
 class TeamCityReporter(
     settings: DiffSettings,
@@ -21,7 +22,9 @@ class TeamCityReporter(
     val suiteName = "Compare dist for JPS"
 
     override fun beginReport() {
-        send(TestSuiteStarted(suiteName))
+        workManager.io {
+            send(TestSuiteStarted(suiteName))
+        }
     }
 
     override fun close() {
@@ -34,84 +37,52 @@ class TeamCityReporter(
     }
 
     private fun send(msg: ServiceMessage) {
-        workManager.io {
-            out.println(msg.asString())
-        }
-    }
-
-    override fun dir(item: Item, body: (DiffContext) -> Unit) {
-        lastName(item.relativePath)
-
-        // wait until all work inside this dir to be done before going to next item
-        // let its done by hooking workManager.submit
-        val todo = mutableListOf<Future<*>>()
-        body(
-            rootContext.copy(
-                workManager = object : IWorkManager {
-                    override fun submit(workTitle: String, work: () -> Unit): Future<*> {
-                        val f = workManager.submit(workTitle, work)
-                        synchronized(todo) {
-                            todo.add(f)
-                        }
-                        return f
-                    }
-                })
-        )
-
-        while (todo.isNotEmpty()) {
-            synchronized(todo) {
-                val copy = todo.toList()
-                todo.clear()
-                copy
-            }.forEach {
-                it.get()
-            }
-        }
+        out.println(msg.asString())
     }
 
     override fun writeItem(item: FileInfo, expected: String?, actual: String?) {
-        val relativePath = item.relativePath
-            .replace(Regex("[^A-Za-z0-9\\-/]"), "_")
-            .replace('/', '.')
-            .trim('.', '_')
+        workManager.io {
+            val relativePath = item.relativePath
+                .replace(Regex("[^A-Za-z0-9\\-/]"), "_")
+                .replace('/', '.')
+                .trim('.', '_')
 
-        if (item.kind == FileKind.DIR) {
-            dirUnpaired = true
-            send(TestStarted(relativePath, false, null))
-            send(
-                when (item.status) {
-                    MISSED -> TestFailed(relativePath, "MISSED")
-                    UNEXPECTED -> TestFailed(relativePath, "UNEXPECTED")
-                    else -> error("DIR cannot be ${item.status}")
-                }
-            )
-        } else {
-            if (!mySettings.reportMatched) {
-                @Suppress("NON_EXHAUSTIVE_WHEN")
-                when (item.status) {
-                    MATCHED, COPY -> return
-                }
-            }
-
-            if (item.status == COPY) {
-                send(TestIgnored(relativePath, item.status.toString()))
-            } else {
+            if (item.kind == FileKind.DIR) {
+                dirUnpaired = true
                 send(TestStarted(relativePath, false, null))
+                send(
+                    when (item.status) {
+                        MISSED -> TestFailed(relativePath, "MISSED")
+                        UNEXPECTED -> TestFailed(relativePath, "UNEXPECTED")
+                        else -> error("DIR cannot be ${item.status}")
+                    }
+                )
+            } else {
+//                if (!mySettings.reportMatched) {
+//                    @Suppress("NON_EXHAUSTIVE_WHEN")
+//                    when (item.status) {
+//                        MATCHED, COPY -> return
+//                    }
+//                }
 
-                when (item.status) {
-                    MATCHED -> Unit
-                    COPY -> Unit
-                    MISSED -> send(TestFailed(relativePath, "MISSED"))
-                    UNEXPECTED -> send(TestFailed(relativePath, "UNEXPECTED"))
-                    MISMATCHED -> send(TestFailed(relativePath, fakeMismatchedException, actual, expected))
+                if (item.status == COPY) {
+                    send(TestIgnored(relativePath, item.status.toString()))
+                } else {
+                    send(TestStarted(relativePath, false, null))
+
+                    when (item.status) {
+                        MATCHED -> Unit
+                        COPY -> Unit
+                        MISSED -> send(TestFailed(relativePath, "MISSED"))
+                        UNEXPECTED -> send(TestFailed(relativePath, "UNEXPECTED"))
+                        MISMATCHED -> send(TestFailed(relativePath, fakeMismatchedException, actual, expected))
+                    }
+
+                    send(TestFinished(relativePath, 0))
                 }
-
-                send(TestFinished(relativePath, 0))
             }
         }
     }
-
-    private fun lastName(path: String) = path.split("/").last()
 
     override fun writeDiff(item: Item, ext: String, outputWriter: (PrintWriter.() -> Unit)?) {
         // do nothing
